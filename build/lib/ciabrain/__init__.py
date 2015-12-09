395d68e590181d94137a954e2acf8ecf741d2fe7
rickyall <783814127@qq.com>
2015-12-09 09:38:14 +0800

    V1.0.1

--- build/lib/ciabrain/__init__.py
+++
@@ -1,2 +0,0 @@
-import qisc
-import yuzw
--- build/lib/ciabrain/qisc/index.py
+++
@@ -1,6 +0,0 @@
-def print_lol(arr):
-    for row in arr:
-        if(isinstance(row , list)):
-            print_lol(row)
-        else:
-            print row
