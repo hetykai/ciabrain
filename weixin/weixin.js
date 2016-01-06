7ade0a80ed433921f41dd2dd185a53c9a36bdd87
rickyall <783814127@qq.com>
2016-01-06 16:30:00 +0800

    gengx

--- weixin/weixin.js
+++ weixin/weixin.js
@@ -22,8 +22,8 @@ module.exports = function(app){
       // 回复屌丝(普通回复)
       var url = 'http://www.tuling123.com/openapi/api?key='+weixinCon["robotKey"]+'&info='+message.Content
       request(url).then(function(response){
-        console.log(response.text);
-        res.reply(response.text);
+        var r = JSON.parse(response);
+        res.reply(r.text);
       });
     }
   }));
